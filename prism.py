f7d602befd6bc5b0e4799ca67b8ed4a90cbecaa7
Rafael Brito <rafaelbritoh.007@gmail.com>
2023-10-31 23:23:27 -0500

    Mejoras en el aspecto de la impresion en consola

--- prism.py
+++ prism.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import random
+from colorama import Fore, Style
+
+# Configurar colores
+COLOR_SUBTITULO = Fore.YELLOW  # Color para los subtítulos (amarillo)
+COLOR_DESTACADO = Fore.CYAN  # Color para los textos destacados (cian)
+RESET_COLOR = Style.RESET_ALL
+
+# Función para imprimir subtítulos en amarillo
+def imprimir_subtitulo(texto, color=COLOR_SUBTITULO):
+    print(color + texto + RESET_COLOR)
 
 # Paso 1: Crear un dataset ficticio
 def crear_dataset():
@@ -33,7 +43,8 @@ def calcular_reglas(df, columns):
 
 # Main
 df = crear_dataset()
-print("Dataset original:\n", df)
+imprimir_subtitulo("DATASET ORIGINAL:")
+print(df)
 
 iteration = 0
 columns_to_check = list(df.columns[:-1])  # Excluyendo 'cerveza'
@@ -41,7 +52,7 @@ reglas_seleccionadas = []
 
 while not df.empty:
     iteration += 1
-    print(f"\nIteración {iteration}:")
+    imprimir_subtitulo(f"\n\nITERACIÓN {iteration}:")
 
     reglas = calcular_reglas(df, columns_to_check)
     
@@ -57,14 +68,15 @@ while not df.empty:
     reglas_seleccionadas.append(f"{regla_seleccionada[0][0]}={regla_seleccionada[0][1]}")
     columns_to_check.remove(regla_seleccionada[0][0])
     
-    print(f"\nRegla seleccionada para la iteración {iteration}:")
+    imprimir_subtitulo(f"\nRegla seleccionada para la iteración {iteration}:", color=COLOR_DESTACADO)
     condition = f"{regla_seleccionada[0][0]}={regla_seleccionada[0][1]}"
     print(f"SI {condition} ENTONCES cerveza=Sí -> Confianza = {regla_seleccionada[1]:.2f} | Soporte = {regla_seleccionada[2]:.2f}")
 
     # Paso 5: Reducir el dataset
     condition = df[regla_seleccionada[0][0]] == regla_seleccionada[0][1]
     df = df[condition]
-    print("\nDataset reducido:\n", df)
+    imprimir_subtitulo("\nDataset reducido:", color=COLOR_DESTACADO)
+    print(df)
 
-print("\n\nReglas seleccionadas:\n")
-print(" AND ".join(reglas_seleccionadas) + " ENTONCES cerveza=true")
+imprimir_subtitulo("\n\nREGLAS SELECCIONADAS:", color=COLOR_DESTACADO)
+print(" AND ".join(reglas_seleccionadas) + " ENTONCES cerveza=true\n")
